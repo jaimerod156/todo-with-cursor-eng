098eacbd6650144820463e9ff5a221d3ba760cc3
jaimerod156 <jaimerodriguez156@gmail.com>
2026-08-27 16:22:19 -0400

    devcontainer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { TodoList } from '@/components/todo-list'
 
 export default function Page() {
-  return <TodoList />
+  return(
+    <div className="font-sans">
+      <TodoList />
+    </div>
+  )
 }
