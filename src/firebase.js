3e9b7b3b25ffe5ae221784e3b73ca959ef22b2f4
jaimerod156 <jaimerodriguez156@gmail.com>
2026-08-29 17:39:28 -0400

    creando credenciales firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0XaVXYX9Q-J2m6J35LHXT8Ko43ldlRVU",
+  authDomain: "v1-013-todo-with-cursor-f0b3b.firebaseapp.com",
+  projectId: "v1-013-todo-with-cursor-f0b3b",
+  storageBucket: "v1-013-todo-with-cursor-f0b3b.firebasestorage.app",
+  messagingSenderId: "436005504375",
+  appId: "1:436005504375:web:d41c5632d2a346f7348d72"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
